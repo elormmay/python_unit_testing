9b2900e32364202db1dda22c5bb0e81db4d7eba1
elormmay <elormmay@gmail.com>
2021-10-23 08:27:38 +0000

    Added test files and modules

---
+++ unittest/calc.py
@@ -0,0 +1,24 @@
+# functions to test
+
+
+def add(a, b):
+    """Add function"""
+    return a + b
+
+
+def sub(a, b):
+    """Subtract function"""
+    return a - b
+
+
+def multiply(a, b):
+    """Multiply function"""
+    return a * b
+
+
+def divide(a, b):
+    """Divide function"""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
