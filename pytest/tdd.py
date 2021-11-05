9d51f797da5a9708d7a90b9bacf7a8fa7839851e
elormmay <elormmay@gmail.com>
2021-11-05 15:19:15 +0000

    TDD: with pytest markers

--- pytest/tdd.py
+++ pytest/tdd.py
@@ -21,3 +21,19 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+
+def custom_num_list(value):
+    return [x for x in range(value)]
+
+
+def custom_func(value, power, const):
+    return const * (value) ** power
+
+
+def custom_non_lin_num_list(length, const, power):
+    return [custom_func(x, const, power) for x in range(length)]
+
+
+# print(custom_num_list(10))
+print(custom_non_lin_num_list(10, 2, 3))
--- pytest/test_tdd.py
+++ pytest/test_tdd.py
@@ -1,25 +1,46 @@
 """Trying TTD"""
 # all test are written before the function
+# used pytest.mark to group tests. a pytest.ini file created in  rootdir shows the custom markers
 import pytest
-from tdd import add, display_message, show_message, check_length, divide
+from tdd import (
+    add,
+    display_message,
+    show_message,
+    check_length,
+    divide,
+    custom_num_list,
+    custom_func,
+)
 
 
 class TestCase:
+    @pytest.mark.calc
     def test_add(self):
         assert add(2, 3) == 5
         assert add(-1, -1) == -2
         assert add(-2, 1) == -1
 
+    @pytest.mark.messages
     def test_display_message(self):
         assert display_message() == "Message works!"
 
+    @pytest.mark.messages
     def test_show_message(self):
         assert show_message("Hello") == "Hello"
 
+    @pytest.mark.calc
     def test_check_length(self):
         assert check_length("hello") == 5
 
+    @pytest.mark.calc
     def test_division(self):
-        # valueError will be raise thus this test passes
+        # valueError will be raised thus this test passes
         with pytest.raises(ValueError):
             assert divide(4, 0) == 2
+
+    def test_custom_num_list(self):
+        assert len(custom_num_list(10)) == 10
+
+    def test_custom_func(self):
+        assert custom_func(3, 2, 3) == 27
+
